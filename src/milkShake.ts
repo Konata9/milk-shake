7c3a9b1a7f3c3965662aa855846689740616c5c5
konata9 <maochichen@163.com>
2019-04-25 23:32:15 +0800

    增加两个基础方法

--- src/milkShake.ts
+++ src/milkShake.ts
@@ -1,13 +1,21 @@
 function snakeToCamel(snake: string): string {
   return snake
     .split("_")
-    .filter((str) => !!str)
-    .map((str, index) =>
-      index > 0 ? str[0].toUpperCase() + str.slice(1) : str
+    .filter((str: string): boolean => !!str)
+    .map(
+      (str: string, index: number): string =>
+        index > 0 ? str[0].toUpperCase() + str.slice(1) : str
     )
     .join("");
 }
 
 function camelToSnake(camel: string): string {
-  return "";
+  const matches: Array<string> = camel.match(/[A-Z]/g) || [];
+  let snake: string = "";
+  matches.forEach(
+    (word: string): void => {
+      snake = camel.replace(word, `_${word.toLocaleLowerCase()}`);
+    }
+  );
+  return snake;
 }
