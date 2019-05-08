343608d5b65f1c46da17cdb91d0d7fb9b0d114e3
konata9 <maochichen@163.com>
2019-05-09 00:36:19 +0800

    搭建基础框架

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
 import typeCheck from "@konata9/typecheck.js";
 import { formatSnakeToCamel, formatCamelToSnake } from "./utils";
 
-function shake(params: { [key: string]: any }, ...rest: [any]): object {
-  const funArray = [...rest];
+interface FormatOptions{
+  method: string | ((key:string) => string)
+  exclude: string[]
 }
+
+function handleArrayValue() {}
+
+function formatKeys(method: string | ((key:string) => string), exclude:string[] = []) : {
+  return (params) =>
+}
+
+function meltKeys() {}
+
+function mapKeys() {}
+
+function shake(params: { [key: string]: any }, ...rest: [any]): object {
+  return [...rest].reduce((prev, current) => {
+    return current(prev);
+  }, params);
+}
+
+export { shake, formatKeys, meltKeys, mapKeys };
+export default {
+  shake,
+  formatKeys,
+  meltKeys,
+  mapKeys
+};
