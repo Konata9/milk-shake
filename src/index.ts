715a475fa5d4a3cfe7b1b3510fc9a26960d5e698
Konata9 <maochichen@163.com>
2019-05-09 11:52:03 +0800

    添加部分方法

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,60 @@
 import typeCheck from "@konata9/typecheck.js";
-import { formatSnakeToCamel, formatCamelToSnake } from "./utils";
+import {formatSnakeToCamel, formatCamelToSnake} from "./utils";
 
-interface FormatOptions{
-  method: string | ((key:string) => string)
-  exclude: string[]
+interface HandleFunction {
+  (params: {[key: string]: any}): {[key: string]: any};
 }
 
 function handleArrayValue() {}
 
-function formatKeys(method: string | ((key:string) => string), exclude:string[] = []) : {
-  return (params) =>
-}
+const checkFormatKeysMethod = (method: any) => {
+  if (!method) {
+    throw new Error("method must hava a value");
+  }
+
+  if (method && !["string", "function"].includes(typeCheck(method))) {
+    throw new Error("method must be a string type or a function");
+  }
+
+  if (
+    method &&
+    typeCheck(method) === "string" &&
+    !["toCamel", "toSnake"].includes(method)
+  ) {
+    throw new Error('method must be "toCamel" or "toSnake"');
+  }
+};
+
+const formatKeys = (
+  method: string | ((key: string) => string),
+  excludes: string[] = []
+): HandleFunction => {
+  checkFormatKeysMethod(method);
+  return (params) => {
+    const copyParams = {...params};
+    const result: {[key: string]: any} = {};
+
+    Object.keys(copyParams).forEach((key) => {
+      if (excludes.includes(key)) {
+        result[key] = copyParams[key];
+      } else {
+      }
+    });
+    return result;
+  };
+};
 
 function meltKeys() {}
 
 function mapKeys() {}
 
-function shake(params: { [key: string]: any }, ...rest: [any]): object {
+function shake(params: {[key: string]: any}, ...rest: [any]): object {
   return [...rest].reduce((prev, current) => {
     return current(prev);
   }, params);
 }
 
-export { shake, formatKeys, meltKeys, mapKeys };
+export {shake, formatKeys, meltKeys, mapKeys};
 export default {
   shake,
   formatKeys,
